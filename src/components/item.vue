5b79ae47870ed247f3c6a04c545d3f3d8dab07e5
José Aljuria <josejavieral13@gmail.com>
2020-04-09 23:21:31 -0400

    modificado el aspecto de la lista

--- src/components/item.vue
+++ src/components/item.vue
@@ -1,7 +1,10 @@
 <template>
-	<li class="row rounded p-2 align-items-center" style="background-color: #333333">
-		<div class="col-8 text-center text-light text-break">
+	<li class="row rounded p-2 align-items-center mb-2" style="background-color: #333333">
+		<div class="col-4 text-center text-light text-break">
 			{{tarea.tarea}}
+		</div>
+		<div class="col-4 text-center text-light text-break">
+			{{tarea.fecha}}
 		</div>
 		<div class="col-4 text-right">
 			<button type="button" class="btn btn-danger" v-on:click="this.eliminar">X</button>
--- src/components/listaTarea.vue
+++ src/components/listaTarea.vue
@@ -1,5 +1,15 @@
 <template>
   <ul>
+	<li>
+		<div class="row text-center font-weight-bold">
+			<div class="col-4">
+				Nombre
+			</div>
+			<div class="col-4">
+				Fecha de Inicio
+			</div>
+		</div>
+	</li>
 	<item v-for="(i, indice) in lista" :key="indice" :tarea="i"  @itemEliminado="itemEliminado"/>
   </ul>
 </template>
